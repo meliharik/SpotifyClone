795207bcea908f54e08902cc9d985fdafae98fb8
Melih Arık <meliharik@hotmail.com>
2022-12-26 19:45:24 +0300

    library screen UI

---
+++ SpotifyClone/Controllers/General/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  SpotifyClone
+//
+//  Created by melih arik on 12/24/22.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemOrange
+    }
+}
---
+++ SpotifyClone/Controllers/General/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  SpotifyClone
+//
+//  Created by melih arik on 12/24/22.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
